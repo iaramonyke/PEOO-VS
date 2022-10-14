fda6813175e6c48879d9b236c45a8acf34d5ef8f
Iara Monyke <i.monyke@academico.ifrn.edu.br>
2022-10-14 17:39:18 -0300

    atualização do exemplo 01

--- aula01/exemploapp/MainWindow.xaml.cs
+++ aula01/exemploapp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace exemploapp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _ = MessageBox.Show("Olá");
+        }
     }
 }
