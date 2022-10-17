65d29dd66129f16f83eb2b9b9ffa1e764a86a011
Iara Monyke <i.monyke@academico.ifrn.edu.br>
2022-10-17 15:53:52 -0300

    upload agenda

---
+++ aula02/AgendaApp/contato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaApp
+{
+    class contato
+    {
+        private string nome, email, fone;
+        public contato(string n, string e, string f)
+        {
+            nome = n;
+            email = e;
+            fone = f;
+        }
+        public override string ToString()
+        {
+            return $"{nome} - {email} - {fone}";
+        }
+    }
+}
