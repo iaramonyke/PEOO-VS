3001acadc1ecdf4ba48ff82e341a9b55e9c257f6
20211011110040 <20211011110040@CNAT407148>
2022-11-11 15:54:30 -0300

    upload bingo app

--- aula 05/BingoApp/MainWindow.xaml.cs
+++ aula 05/BingoApp/MainWindow.xaml.cs
@@ -29,14 +29,34 @@ namespace BingoApp
         {
             txtNumBolas.Text = slider.Value.ToString();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Iniciar");
-        }
+        private Bingo b;
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Sortear");
+            int x = b.Sortear();
+            if (x == -1)
+            {
+                txtNum.Text = "Fim";
+                btnIniciar.IsEnabled = true;
+                btnSortear.IsEnabled = false;
+            }
+            else
+            {
+                txtNum.Text = x.ToString();
+                string s = "";
+                foreach (int i in b.Sorteados())
+                    s = s + i + " - ";
+                txtSorteados.Text = s;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            b = new Bingo();
+            b.Iniciar(int.Parse(txtNumBolas.Text));
+            btnIniciar.IsEnabled = false;
+            btnSortear.IsEnabled = true;
+            txtNum.Clear();
+            txtSorteados.Clear();
         }
     }
 }
